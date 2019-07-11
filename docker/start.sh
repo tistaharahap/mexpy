a63c083d0ef8d71dbb330d48a97f6e8b78e8a578
Rifky Ali <rifky.ali.kiki@gmail.com>
2019-07-12 01:42:47 +0700

    add short

--- docker/start.sh
+++ docker/start.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -e
 
 case $1 in
-  "run")
+  "long")
     shift
     python app.py
     ;;
+  "short")
+    shift
+    python app-short.py
+    ;;
   *)
-    echo "usage: $0 [run]"
+    echo "usage: $0 [long|short]"
     exit 1
     ;;
 esac
